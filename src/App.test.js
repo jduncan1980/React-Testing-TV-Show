21f13dbbbdd7c5813c47484fee8d175ac78a98e5
Jason Duncan <jasonduncan@Jasons-MacBook-Pro.local>
2020-08-06 00:38:00 -0400

    mvp

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, wait, cleanup, fireEvent } from '@testing-library/react';
+import { render, waitFor, cleanup, fireEvent } from '@testing-library/react';
 import App from './App';
 import { fetchShows as mockFetchShows } from './api/fetchShows';
 import mockData from './fixtures/mockData';
@@ -18,16 +18,34 @@ test('renders app', () => {
 
 test('updates show data on render', async () => {
 	mockFetchShows.mockResolvedValueOnce({ data: mockData });
-	const { getByText, getByTestId } = render(<App />);
+	const {
+		getByText,
+		getByTestId,
+		queryByTestId,
+		getAllByTestId,
+		queryAllByTestId,
+		findByTestId,
+	} = render(<App />);
 
-	await wait();
+	await waitFor(() => {
+		expect(getByTestId('name')).toHaveTextContent(/stranger things/i);
+		expect(queryByTestId('episode')).not.toBeInTheDocument();
+	});
 	expect(getByTestId('name')).toHaveTextContent(/stranger things/i);
-	expect(getByTestId('episode')).not.toBeInTheDocument();
 
 	const dropdown = getByText(/select a season/i);
 	dropdown.value = 'Season 1';
 	expect(dropdown.value).toBe('Season 1');
 	fireEvent.click(dropdown);
-	await wait();
-	// expect(getByText(/Season 1, Episode 1/i)).toBeInTheDOM();
+	await waitFor(() => {
+		expect(dropdown.value).toBe('Season 1');
+		// expect(queryByTestId('episode')).toBeInTheDocument();
+	});
+
+	dropdown.value = 'Season 2';
+	expect(dropdown.value).toBe('Season 2');
+	fireEvent.click(dropdown);
+	await waitFor(() => {
+		expect(dropdown.value).toBe('Season 2');
+	});
 });
