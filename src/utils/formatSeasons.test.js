cb5e9f94f4fede0fa5a5aa969069b280a5609864
Jason Duncan <jasonduncan@Jasons-MacBook-Pro.local>
2020-08-07 17:24:53 -0400

    fixed formatSeasons test

--- src/utils/formatSeasons.test.js
+++ src/utils/formatSeasons.test.js
@@ -1,16 +1,13 @@
 import { formatSeasons } from './formatSeasons';
-// import { fetchShows as mockFetchShows } from '../api/fetchShows';
 import mockData from '../fixtures/mockData';
-
-jest.mock('../api/fetchShows.js');
 
 test('formats seasons correctly', () => {
 	const formatted = formatSeasons(mockData._embedded.episodes);
-	console.log(formatted);
+	console.log(formatted['Season 1']);
 
-	expect(formatted['Season 3:'].length).toBe(8);
-	expect(formatted['Season 1:'][0].id).toBe(539465);
-	expect(formatted['Season 2:'][1].name).toBe(
+	expect(formatted['Season 3'].length).toBe(8);
+	expect(formatted['Season 1'][0].id).toBe(553946);
+	expect(formatted['Season 2'][1].name).toBe(
 		'Chapter Two: Trick or Treat, Freak'
 	);
 });
